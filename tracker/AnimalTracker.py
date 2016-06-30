a92739cca72fa8e58c95064ab3383e7626a8aa70
Fabian Gutierrez <fagossa@gmail.com>
2016-06-30 13:57:46 +0200

    improves image cropping

--- tracker/AnimalTracker.py
+++ tracker/AnimalTracker.py
@@ -5,13 +5,13 @@ import tracker.AOITracker
 class AnimalTracker:
     """ Handle the animal detection and recognition """
 
-    def __init__(self, windowName, width, height):
+    def __init__(self, windowName, max_width, max_height):
         print "AnimalTracker::__init__"
         self.windowName = windowName
         # create switch for ON/OFF functionality
         self.switch = '0 : OFF \n1 : ON'
         self.aoi = tracker.AOITracker(self.windowName, self.switch)
-        self.aoi.createTrackers(width, height)
+        self.aoi.createTrackers(max_width, max_height)
 
     def hasStarted(self):
         return self.aoi.trackSwitchValue()
@@ -24,6 +24,11 @@ class AnimalTracker:
         self.y1 = y
         self.x2 = x + width
         self.y2 = y + height
+        self.crop_img = img[self.y1:self.y2, self.x1:self.x2]
 
     def display(self):
+        # shows the aoi
+        width, height = self.crop_img.shape[:2]
+        if width > 0 and height > 0:
+            cv2.imshow("aoi", self.crop_img)
         cv2.rectangle(self.img, (self.x1, self.y1), (self.x2, self.y2), self.c, 0)
