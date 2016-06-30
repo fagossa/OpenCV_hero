fc92e82d96cfb2b16aad556d9d798bc0a0afdef6
Fabian Gutierrez <fagossa@gmail.com>
2016-06-30 13:18:24 +0200

    another refactoring

--- tracker/AnimalTracker.py
+++ tracker/AnimalTracker.py
@@ -4,17 +4,43 @@ import cv2
 class AnimalTracker:
     """ Handle the animal detection and recognition """
 
-    def __init__(self, imgName):
+    def __init__(self, windowName):
         print "AnimalTracker::__init__"
-        self.imgName = imgName
+        self.windowName = windowName
+        # create switch for ON/OFF functionality
+        self.switch = '0 : OFF \n1 : ON'
 
-    def update(self, img, area_x, area_y, area_width, area_height):
+    def createTrackers(self, width, height):
+        """ create trackbars for color change """
+        cv2.createTrackbar('posx', self.windowName, 0, width, self.nothing)
+        cv2.createTrackbar('posy', self.windowName, 0, height, self.nothing)
+        cv2.createTrackbar('width', self.windowName, 0, width, self.nothing)
+        cv2.createTrackbar('height', self.windowName, 0, height, self.nothing)
+        cv2.createTrackbar(self.switch, self.windowName, 0, 1, self.nothing)
+
+    def trackSwitchValue(self):
+        return cv2.getTrackbarPos(self.switch, self.windowName)
+
+    def trackValues(self):
+        """ get values from trackers """
+        x = cv2.getTrackbarPos('posx', self.windowName)
+        y = cv2.getTrackbarPos('posy', self.windowName)
+        width = cv2.getTrackbarPos('width', self.windowName)
+        height = cv2.getTrackbarPos('height', self.windowName)
+        return (x, y, width, height)
+
+    def nothing(self, param):
+        pass
+
+    def update(self, img):
+        (x, y, width, height) = self.trackValues()
         self.img = img
         self.c = (0, 0, 255)
-        self.x = area_x
-        self.y = area_y
-        self.w = area_x + area_width
-        self.h = area_y + area_height
+        self.x1 = x
+        self.y1 = y
+        self.x2 = x + width
+        self.y2 = y + height
+        print "AnimalTracker::x:{0} y:{1}, w:{2} h:{3}".format(str(self.x1), str(self.y1), str(self.x2), str(self.y2))
 
     def display(self):
-        cv2.rectangle(self.img, (self.x, self.y), (self.w, self.h), self.c, 0)
+        cv2.rectangle(self.img, (self.x1, self.y1), (self.x2, self.y2), self.c, 0)
--- tracker/Window.py
+++ tracker/Window.py
@@ -8,15 +8,14 @@ class Window:
 
     def __init__(self, imgName):
         print "Window::__init__"
-        cv2.namedWindow('image')
-        # create switch for ON/OFF functionality
-        self.switch = '0 : OFF \n1 : ON'
+        self.windowName = 'image'
+        cv2.namedWindow(self.windowName)
         self.actualImage = cv2.imread(imgName)
         self.updateDimensions(self.actualImage)
         print "Window::found image having w:{0} h:{1}".format(str(self.width), str(self.height))
         self.emptyImage = np.zeros((self.width, self.height, 3), np.uint8)
-        self.tracker = tracker.AnimalTracker(imgName)
-        self.createTrackers("image")
+        self.tracker = tracker.AnimalTracker(self.windowName)
+        self.tracker.createTrackers(self.width, self.height)
         self.display(1)
         print "Window::bye"
 
@@ -25,26 +24,6 @@ class Window:
         self.height = height
         self.width = width
 
-    def createTrackers(self, name):
-        """ create trackbars for color change """
-        cv2.createTrackbar('posx', 'image', 0, self.width, self.nothing)
-        cv2.createTrackbar('posy', 'image', 0, self.height, self.nothing)
-        cv2.createTrackbar('width', 'image', 0, self.width, self.nothing)
-        cv2.createTrackbar('height', 'image', 0, self.height, self.nothing)
-        cv2.createTrackbar(self.switch, 'image', 0, 1, self.nothing)
-
-    def trackValues(self, switch):
-        """ get values from trackers """
-        x = cv2.getTrackbarPos('posx', 'image')
-        y = cv2.getTrackbarPos('posy', 'image')
-        width = cv2.getTrackbarPos('width', 'image')
-        height = cv2.getTrackbarPos('height', 'image')
-        s = cv2.getTrackbarPos(switch, 'image')
-        return (x, y, width, height, s)
-
-    def nothing(self, param):
-        pass
-
     def display(self, condition):
         print "Window::displayLoop"
         while(condition):
@@ -52,15 +31,13 @@ class Window:
             if k == 27:
                 break
 
-            overlay = self.actualImage.copy()
-            # get current trackbars positions
-            (area_x, area_y, area_w, area_h, s) = self.trackValues(self.switch)
-
+            s = self.tracker.trackSwitchValue()
             if s == 0:
                 temp = self.emptyImage
-                cv2.imshow('image', temp.copy())
+                cv2.imshow(self.windowName, temp.copy())
             else:
-                self.tracker.update(overlay, area_x, area_y, area_w, area_h)
+                overlay = self.actualImage.copy()
+                self.tracker.update(overlay)
                 self.tracker.display
-                cv2.imshow('image', overlay)
+                cv2.imshow(self.windowName, overlay)
         cv2.destroyAllWindows()
